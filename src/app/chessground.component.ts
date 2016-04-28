03e58a9c4f22cf445750a47a2aa913ed0296a7d7
oddskill <oddskill@gmail.com>
2016-04-28 21:17:14 +0200

    moved styles to component
    moved styles to component Input decorators

--- src/app/chessground.component.ts
+++ src/app/chessground.component.ts
@@ -1,33 +1,35 @@
 
 var Chessground = require('chessground');
-import {Component} from 'angular2/core';
-import {OnInit} from 'angular2/core'
+var uuid = require('uuid');
+import {Component, Input} from 'angular2/core';
+import {AfterViewInit} from 'angular2/core'
 
 
 @Component({
     selector: 'chessground',
-    template:  `<div id='ground1' class="cburnett wood"></div>`,
-   
+    template: `<div [id]="muuid"  [style.width.px]="width" [style.height.px]="height" [ngClass]="[pieces,board]" ></div>`,
+
 })
-export class ChessGroundComponent implements OnInit{
-    
-    gound :any; 
-    ngOnInit()
-    {
-        var options = {
-    orientation: 'white' 
-    };
-    
+export class ChessGroundComponent implements AfterViewInit {
+
+    ground: any;
+    @Input() pieces: string = 'merida';
+    @Input() board: string = 'wood';
+    @Input() muuid: string = uuid.v4();
+    @Input() width: string ='400';
+    @Input() height: string ='400';
     
 
- this.gound = Chessground(document.getElementById("ground1"), options);
-    }; 
-    constructor()
-    {
-        
-        
-        
+    ngAfterViewInit() {
+        var options = {
+            orientation: 'white'
+        };
+
+        this.ground = Chessground(document.getElementById(this.muuid), options);
+    };
+    constructor() {
+
+
     }
-    
- }
-  
+
+}
