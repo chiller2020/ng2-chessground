3b88566c62540b98524ab495491de978c3d870a2
oddskill <oddskill@gmail.com>
2016-05-07 11:06:23 +0200

    add getter and setter
    add getGround() to retrieve chessground instance
    add setGround(options: any) to set chessground options

--- src/app/chessground.component.ts
+++ src/app/chessground.component.ts
@@ -8,29 +8,38 @@ import {AfterViewInit} from '@angular/core'
 @Component({
     selector: 'chessground',
     template: `<div [id]="muuid"  [style.width]="width" [style.height]="height" [ngClass]="[pieces,board]"></div>`
-    
+
 })
 export class ChessGroundComponent implements AfterViewInit {
 
     ground: any;
     @Input() pieces: string = 'staunton';
     @Input() board: string = 'wood';
-    @Input() muuid: string = uuid.v4(); 
-    @Input() width: string = '100%'; 
-    @Input() height: string = '100%'; 
-    
-    ngAfterViewInit() {   
-        var options = { 
-            orientation: 'white'  
+    @Input() muuid: string = uuid.v4();
+    @Input() width: string = '100%';
+    @Input() height: string = '100%';
+
+    ngAfterViewInit() {
+
+        var options = {
+            orientation: 'white'
         };
 
         this.ground = Chessground(document.getElementById(this.muuid), options);
     };
 
+    getGround() {
+        return this.ground;
+    };
+    
+    setOptions(options: any)
+    {
+        this.ground.setOptions(options);
+    }
+
     constructor() {
 
-    
+
     };
 
 }
- 
