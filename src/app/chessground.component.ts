c272800009cd24605ae0a6d52a4ce2b27c16f22a
oddskill <oddskill@gmail.com>
2016-05-14 19:51:34 +0200

    add input property for orientation
    add input property for orientation
    code refactoring

--- src/app/chessground.component.ts
+++ src/app/chessground.component.ts
@@ -15,71 +15,69 @@ export class ChessGroundComponent implements AfterViewInit {
 
 
     uuid = require('uuid');
-    
-    @Input() pieces: string ='merida';
+
+    @Input() pieces: string = 'merida';
     @Input() board: string = 'wood';
     @Input() muuid: string = this.uuid.v4();
     @Input() width: string = '100%';
     @Input() height: string = '100%';
     @Input() options: any;
+    @Input() orientation: any = 'white';
 
     @Output() change: EventEmitter<any> = new EventEmitter();
     @Output() move: EventEmitter<any> = new EventEmitter();
     @Output() select: EventEmitter<any> = new EventEmitter();
 
     ground: any;
-    private defaultOptions: any = {
-        orientation: 'white',
-        coordinates: true,
-
-        draggable: {
-            enabled: true,        // allow moves & premoves to use drag'n drop
-            distance: 3,          // minimum distance to initiate a drag, in pixels
-            squareTarget: false,  // display big square target; intended for mobile
-            centerPiece: true,    // center the piece on cursor at drag start
-            showGhost: true,      // show ghost of piece being dragged
-        },
-        drawable: {
-          enabled: true         // enable SVG circle and arrow drawing on the board
-        },  
-        events: {
-            change: function () {this.onChange() }.bind(this),   // called after the situation changes on the board
-            // called after a piece has been moved.
-            // capturedPiece is null or like {color: 'white', 'role': 'queen'}
-            move: function (orig, dest, capturedPiece) {this.onMove(orig,dest,capturedPiece) }.bind(this),
-            select: function (key) { this.onSelect(key) }.bind(this) // called when a square is selected
-        }
-
-    };
+    private defaultOptions: any;
 
     constructor(private cgctrl: ChessGroundControlService) {
 
     }
 
-    onChange()
-    {
-        console.log('changed');
-      
+    onChange() {
+
     }
-    
-    onMove(orig, dest, capturedPiece)
-    {
-        console.log('onmove');
-        console.log(orig);
-        console.log(dest);
-        console.log(capturedPiece);
-        
-        
-       
-    }   
-    onSelect(key)
-    {
-      console.log('onselect');
-      console.log(key);
-        
+
+    onMove(orig, dest, capturedPiece) {
+
+
     }
-    
+    onSelect(key) {
+
+
+    }
+
     ngAfterViewInit() {
+
+        this.defaultOptions = {
+            orientation: this.orientation,
+            coordinates: true,
+
+            draggable: {
+                enabled: true,        // allow moves & premoves to use drag'n drop
+                distance: 3,          // minimum distance to initiate a drag, in pixels
+                squareTarget: false,  // display big square target; intended for mobile
+                centerPiece: true,    // center the piece on cursor at drag start
+                showGhost: true,      // show ghost of piece being dragged
+            },
+            drawable: {
+                enabled: true         // enable SVG circle and arrow drawing on the board
+            },
+            events: {
+                change: function () { this.onChange() }.bind(this),   // called after the situation changes on the board
+                // called after a piece has been moved.
+                // capturedPiece is null or like {color: 'white', 'role': 'queen'}
+                move: function (orig, dest, capturedPiece) { this.onMove(orig, dest, capturedPiece) }.bind(this),
+                select: function (key) { this.onSelect(key) }.bind(this) // called when a square is selected
+            },
+            movable: {
+                free: true,           // all moves are valid - board editor
+                color: "both",        // color that can move. "white" | "black" | "both" | null
+                dropOff: "trash"    // when a piece is dropped outside the board. "revert" | "trash"
+            }
+
+        };
 
         this.ground = this.cgctrl.init(this.muuid, this.defaultOptions);
 
