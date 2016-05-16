b42911732dde974f8f594cb6781e186f9ca9cfc3
oddskill <oddskill@gmail.com>
2016-05-16 19:36:07 +0200

    format code
    format code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,11 @@ import {ChessGroundPgnViewerComponent} from './chessground-pgnviewer.component'
 @Component({
     selector: 'app',
     template: ` <chessground-pgnviewer [style.width]="'100%'" [style.height]="'100%'"></chessground-pgnviewer>
+                `,
+    directives: [ChessGroundPgnViewerComponent, ChessGroundEditorComponent]
 })
 export class AppComponent { }
 
 
+
+//`<chessground-editor [style.width]="'100%'" [style.height]="'100%'"></chessground-editor>`
