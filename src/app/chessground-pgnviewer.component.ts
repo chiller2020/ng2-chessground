44f5f3ea3efc8dd670fb260e1afd28bd14e2f805
oddskill <oddskill@gmail.com>
2016-05-17 15:57:56 +0200

    initial commit
    initial commit

--- src/app/chessground-pgnviewer.component.ts
+++ src/app/chessground-pgnviewer.component.ts
@@ -20,15 +20,20 @@ import { ChessJsService } from './chessjs.service';
 export class ChessGroundPgnViewerComponent implements AfterViewInit {
 
   private pgn: string = `
+[Event "IBM Kasparov vs. Deep Blue Rematch"]
+[Site "New York, NY USA"]
+[Date "1997.05.11"]
+[Round "6"]
+[White "Deep Blue"]
+[Black "Kasparov, Garry"]
+[Opening "Caro-Kann: 4...Nd7"]
+[ECO "B17"]
+[Result "1-0"]
  
-1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 {This opening is called the Ruy Lopez.}
-4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 O-O 9. h3 Nb8  10. d4 Nbd7
-11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5
-Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6
-23. Ne5 Rae8 24. Bxf7+ Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5
-hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5
-35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6
-Nf2 42. g4 Bd3 43. Re6 1/2-1/2`;
+1.e4 c6 2.d4 d5 3.Nc3 dxe4 4.Nxe4 Nd7 5.Ng5 Ngf6 6.Bd3 e6 7.N1f3 h6
+8.Nxe6 Qe7 9.O-O fxe6 10.Bg6+ Kd8 {Kasparov schüttelt kurz den Kopf} 
+11.Bf4 b5 12.a4 Bb7 13.Re1 Nd5 14.Bg3 Kc8 15.axb5 cxb5 16.Qd3 Bc6 
+17.Bf5 exf5 18.Rxe7 Bxe7 19.c4 1-0`;
        
   private chessjs :any;
   
---
+++ src/app/pgninputtext.component.ts
@@ -0,0 +1,13 @@
+
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'pgninputtext',
+    template: ` <textarea rows="4" cols="50">
+                    test
+                 </textarea> 
+                `,
+ })
+export class PgnInputText { }
+
